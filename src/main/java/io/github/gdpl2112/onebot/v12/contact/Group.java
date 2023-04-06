6634c4ceccd51cc10197f2f9b3be480f800ec39b
HRS-Computer <3474006766@qq.com>
2023-04-06 14:24:40 +0800

    update

--- src/main/java/io/github/gdpl2112/onebot/v12/contact/Group.java
+++ src/main/java/io/github/gdpl2112/onebot/v12/contact/Group.java
@@ -3,7 +3,6 @@ package io.github.gdpl2112.onebot.v12.contact;
 import com.alibaba.fastjson.annotation.JSONField;
 
 /**
- * <table><thead><tr><th style="text-align: center;">字段名</th><th style="text-align: center;">数据类型</th><th style="text-align: center;">说明</th></tr></thead><tbody><tr><td style="text-align: center;"><code>group_id</code></td><td style="text-align: center;">string</td><td style="text-align: center;">群 ID</td></tr><tr><td style="text-align: center;"><code>group_name</code></td><td style="text-align: center;">string</td><td style="text-align: center;">群名称</td></tr><tr><td style="text-align: center;"><code>wx.avatar</code></td><td style="text-align: center;">string</td><td style="text-align: center;"><code>拓展字段:</code>群头像url</td></tr></tbody></table>
  *
  * @author github.kloping
  */
